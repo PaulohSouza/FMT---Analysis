8d85f5cac232470c13608a50495d7507f2175c70
Paulo Henrique Nascimento de Souza <41805052+PaulohSouza@users.noreply.github.com>
2022-02-08 15:20:10 -0400

    Add files via upload

--- init.R
+++ init.R
@@ -3,10 +3,15 @@ my_packages = c("shiny", "data.table", "ExpDes.pt", "rmarkdown", "plotly",
 "multcompView", "dplyr", "easyanova", "xlsx", "correlation", "factoextra", "Hmisc", "see", "corrplot",
 "RColorBrewer", "highcharter", "grDevices", "car")
 
+
+my_packages
+
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
+    install.packages(p, dependencies=FALSE)
   }
 }
 
 invisible(sapply(my_packages, install_if_missing))
+
+insta
