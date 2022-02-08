80e29899d0376781138e76300c59901d7b33b858
Paulo Henrique Nascimento de Souza <41805052+PaulohSouza@users.noreply.github.com>
2022-02-08 08:23:37 -0400

    init

---
+++ init.R
@@ -0,0 +1,13 @@
+my_packages = c("randomForest", "data.table", "shiny", "ExpDes.pt", "rmarkdown", "labestdata", "plotly",
+"xtable", "shinythemes", "pracma", "stringr", "ds", "car", "tidyverse", "pacman", "agricolae", "ggthemes",
+"multcompView", "dply", "easyanova", "xlsx", "correlation", "factoextra", "ggraph", "Hmisc", "see", "corrplot".
+"RColorBrewer", "highcarter", "grDevices", "data.table")
+
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
