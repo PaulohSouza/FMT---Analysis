6e5f665cb9356453fed738cd4a5c1e92a47debfa
Paulo Henrique Nascimento de Souza <41805052+PaulohSouza@users.noreply.github.com>
2022-02-08 13:53:50 -0400

    Teste

--- init.R
+++ init.R
@@ -1,10 +1,9 @@
-my_packages = c("shiny")
-  
-#"data.table", "ExpDes.pt", "rmarkdown", "plotly",
-#"shinythemes", "pracma", "stringr", "ds", "tidyverse", "pacman", "agricolae", "ggthemes",
-#"multcompView", "dplyr", "easyanova", "xlsx", "correlation", "factoextra", "Hmisc", "see", "corrplot",
-#"RColorBrewer", "highcharter", "grDevices", "car")
+init.R
+#
+# Example R code to install packages if not already installed
+#
 
+my_packages = c("shiny")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
@@ -13,5 +12,3 @@ install_if_missing = function(p) {
 }
 
 invisible(sapply(my_packages, install_if_missing))
-
-insta
