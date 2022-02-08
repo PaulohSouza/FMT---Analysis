696dc5bd9e59ccb55f1a0caff80b5cb6188e43ef
Paulo Henrique Nascimento de Souza <41805052+PaulohSouza@users.noreply.github.com>
2022-02-08 14:56:07 -0400

    Update init.R

--- init.R
+++ init.R
@@ -3,9 +3,6 @@ my_packages = c("shiny", "data.table", "ExpDes.pt", "rmarkdown", "plotly",
 "multcompView", "dplyr", "easyanova", "xlsx", "correlation", "factoextra", "Hmisc", "see", "corrplot",
 "RColorBrewer", "highcharter", "grDevices", "car")
 
-
-my_packages
-
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
@@ -13,5 +10,3 @@ install_if_missing = function(p) {
 }
 
 invisible(sapply(my_packages, install_if_missing))
-
-insta
